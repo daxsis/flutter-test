437d97aae7543b6ef32703d7e2a13fe91ffa971d
Aleksandr Vinokhodov <vinokhodovalex@gmail.com>
2022-07-16 13:42:30 -0400

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(const MyFistApp());
+
+class MyFistApp extends StatelessWidget {
+  const MyFistApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo,
         appBar: AppBar(
           title: const Text("My first app"),
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text(
-            "This is my homapge",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+        body: Center(
+            child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              LinearProgressIndicator(value: 23),
+              Text(
+                '23 %',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              Text(
+                'Press button to download',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              )
+            ],
           ),
+        )),
+        floatingActionButton: const FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.cloud_download),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
