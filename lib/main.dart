cd85e21b028dab76b4f9251b7fa0f46c0ffe4757
Aleksandr Vinokhodov <vinokhodovalex@gmail.com>
2022-07-16 20:27:57 -0400

    adding static assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,30 @@
-import 'dart:async';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyFirstApp());
+void main() => runApp(const MyApp());
 
-class MyFirstApp extends StatefulWidget {
-  const MyFirstApp({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return _MyFistAppState();
-  }
-}
-
-class _MyFistAppState extends State<MyFirstApp> {
-  late bool _loading;
-  late double _progressValue;
-
-  @override
-  void initState() {
-    _loading = false;
-    _progressValue = 0;
-    super.initState();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: const Text("My first app"),
-          centerTitle: true,
+          title: const Text('Adding Assets'),
         ),
         body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            child: _loading
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      LinearProgressIndicator(value: _progressValue),
-                      Text(
-                        '${(_progressValue * 100).round()}%',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      )
-                    ],
-                  )
-                : const Text(
-                    'Press button to download',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              const Image(
+                image: AssetImage('assets/images/22867546-bg.jpg'),
+              ),
+              Image.asset('assets/icons/22867548-icon.png'),
+            ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-            });
-            _updateProgress();
-          },
-          child: const Icon(Icons.cloud_download),
         ),
       ),
     );
-  }
-
-  void _updateProgress() {
-    const oneSec = Duration(seconds: 1);
-    Timer.periodic(oneSec, (Timer t) {
-      setState(() {
-        _progressValue += 0.2;
-
-        if (_progressValue.toStringAsFixed(1) == '1.0') {
-          _loading = false;
-          t.cancel();
-          _progressValue = 0;
-          return;
-        }
-      });
-    });
   }
 }
